d790f29d8006e5fe143b729a46bfdbf15756fe8e
valerio1694-netizen <valerio1694@gmail.com>
2025-12-25 02:28:47 +0100

    Update panel.ino

--- firmware/panel/panel.ino
+++ firmware/panel/panel.ino
@@ -3,105 +3,76 @@
 #include <Update.h>
 #include <TFT_eSPI.h>
 
-// ===== AP Daten =====
-static const char* AP_SSID = "ESP32-OTA";
-static const char* AP_PASS = "12345678";
+#define TFT_BL 32   // Backlight
 
-// ===== Webserver =====
+TFT_eSPI tft = TFT_eSPI();
 WebServer server(80);
 
-// ===== TFT =====
-TFT_eSPI tft = TFT_eSPI();
+// -------- OTA WEB UI ----------
+const char* AP_SSID = "ESP32-OTA";
+const char* AP_PASS = "12345678";
 
-// ===== OTA Seite =====
-static const char* OTA_PAGE =
-"<html><body>"
-"<h2>ESP32 Web OTA</h2>"
-"<p>WLAN: <b>ESP32-OTA</b> (Pass: 12345678)</p>"
-"<p>OTA: <a href='/update'>/update</a></p>"
-"<form method='POST' action='/update' enctype='multipart/form-data'>"
-"<input type='file' name='update' accept='.bin'>"
-"<input type='submit' value='Upload'>"
-"</form>"
-"</body></html>";
-
-void handleRoot() {
-  server.send(200, "text/html", OTA_PAGE);
-}
-
-void handleUpdateUpload() {
-  HTTPUpload& upload = server.upload();
-
-  if (upload.status == UPLOAD_FILE_START) {
-    Update.begin(UPDATE_SIZE_UNKNOWN);
-  } else if (upload.status == UPLOAD_FILE_WRITE) {
-    Update.write(upload.buf, upload.currentSize);
-  } else if (upload.status == UPLOAD_FILE_END) {
-    if (Update.end(true)) {
-      server.send(200, "text/plain", "OK. Rebooting...");
-      delay(300);
-      ESP.restart();
-    } else {
-      server.send(500, "text/plain", "FAIL");
-    }
-  }
-}
-
-void drawDisplayTest() {
-  // Wenn SPI/CS/DC/RST stimmt, MUSS man die Flächen sehen
-  tft.fillScreen(TFT_RED);
-  delay(250);
-  tft.fillScreen(TFT_GREEN);
-  delay(250);
-  tft.fillScreen(TFT_BLUE);
-  delay(250);
-  tft.fillScreen(TFT_BLACK);
-
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  tft.setTextSize(2);
-  tft.setCursor(10, 10);
-  tft.println("DISPLAY TEST");
-  tft.println("ST7796 (TFT_eSPI)");
-
-  tft.setCursor(10, 70);
-  tft.print("AP: ");
-  tft.println(AP_SSID);
-
-  tft.setCursor(10, 95);
-  tft.print("IP: ");
-  tft.println(WiFi.softAPIP());
-
-  tft.setCursor(10, 120);
-  tft.println("OTA: /update");
-
-  // Farb-Balken unten
-  int w = tft.width() / 5;
-  int y = tft.height() - 40;
-  tft.fillRect(0*w, y, w, 40, TFT_RED);
-  tft.fillRect(1*w, y, w, 40, TFT_GREEN);
-  tft.fillRect(2*w, y, w, 40, TFT_BLUE);
-  tft.fillRect(3*w, y, w, 40, TFT_YELLOW);
-  tft.fillRect(4*w, y, w, 40, TFT_MAGENTA);
+String page() {
+  return R"rawliteral(
+<!DOCTYPE html><html>
+<body>
+<h2>ESP32 OTA</h2>
+<form method="POST" action="/update" enctype="multipart/form-data">
+<input type="file" name="update">
+<input type="submit" value="Update">
+</form>
+</body></html>
+)rawliteral";
 }
 
 void setup() {
   Serial.begin(115200);
 
-  // AP starten (damit IP sofort da ist für Displaytext)
+  pinMode(TFT_BL, OUTPUT);
+  digitalWrite(TFT_BL, HIGH);
+
+  // --- WiFi AP ---
   WiFi.mode(WIFI_AP);
   WiFi.softAP(AP_SSID, AP_PASS);
   delay(200);
 
-  // TFT
+  // --- TFT ---
   tft.init();
-  tft.setRotation(1); // Querformat 480x320 bei ST7796-480x320 Panels
-  tft.setAddrWindow(0, 0, 480, 320);
-  drawDisplayTest();
+  tft.setRotation(1);     // Querformat 480x320
+  tft.fillScreen(TFT_BLACK);
 
-  // Web / OTA
-  server.on("/", HTTP_GET, handleRoot);
-  server.on("/update", HTTP_GET, handleRoot);
-  server.on("/update", HTTP_POST, []() { server.send(200); }, handleUpdateUpload);
+  // Farbtest (GANZE FLÄCHE!)
+  tft.fillRect(0,   0, 96, 320, TFT_RED);
+  tft.fillRect(96,  0, 96, 320, TFT_GREEN);
+  tft.fillRect(192, 0, 96, 320, TFT_BLUE);
+  tft.fillRect(288, 0, 96, 320, TFT_YELLOW);
+
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.setTextSize(2);
+  tft.setCursor(10, 10);
+  tft.println("ST7796 OK");
+
+  // --- OTA ---
+  server.on("/", HTTP_GET, []() {
+    server.send(200, "text/html", page());
+  });
+
+  server.on("/update", HTTP_POST,
+    []() {
+      server.send(200, "text/plain", Update.hasError() ? "FAIL" : "OK");
+      ESP.restart();
+    },
+    []() {
+      HTTPUpload& upload = server.upload();
+      if (upload.status == UPLOAD_FILE_START)
+        Update.begin();
+      else if (upload.status == UPLOAD_FILE_WRITE)
+        Update.write(upload.buf, upload.currentSize);
+      else if (upload.status == UPLOAD_FILE_END)
+        Update.end(true);
+    }
+  );
+
   server.begin();
 }
 
